bf22691896d36fe94777ff7c1308f30af7eacb4c
dustinjkeeling <dustinkeeling@gmail.com>
2017-12-31 23:18:22 -0500

    Added Nav

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { RestProvider } from '../../providers/rest/rest';
+import { AddDataPage } from '../add-data/add-data';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+@Page({
+  templateUrl: 'build/pages/add-data/add-data.html'
+})
 export class HomePage {
+
+  addPage = AddDataPage;
 
 	locations: any;
 
